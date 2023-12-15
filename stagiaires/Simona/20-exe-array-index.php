b8dca204f4f55047fa47d187bbbad76fd284d752
simonaiuliana <Iuliana.hoater@cf2000.onmicrosoft.com>
2023-12-15 10:03:40 +0100

    mogification ex 20

--- stagiaires/Simona/20-exe-array-index.php
+++ stagiaires/Simona/20-exe-array-index.php
@@ -2,14 +2,14 @@
 
 
 $monTab = ['un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf', 'dix'];
-echo $element[0] . '<hr>';
-echo $element[1] . '<hr>';
-echo $element[2] . '<hr>';
-echo $element[3] . '<hr>';
-echo $element[4] . '<hr>';
-echo $element[5] . '<hr>';
-echo $element[6] . '<hr>';
-echo $element[7] . '<hr>';
-echo $element[8] . '<hr>';
-echo $element[9] . '<hr>';
+echo $monTab[0] . '<hr>';
+echo $monTab[1] . '<hr>';
+echo $monTab[2] . '<hr>';
+echo $monTab[3] . '<hr>';
+echo $monTab[4] . '<hr>';
+echo $monTab[5] . '<hr>';
+echo $monTab[6] . '<hr>';
+echo $monTab[7] . '<hr>';
+echo $monTab[8] . '<hr>';
+echo $monTab[9] . '<hr>';
 ?>
