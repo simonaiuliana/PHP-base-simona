ebf314922b039326770419a1af82b2463cd07065
simonaiuliana <Iuliana.hoater@cf2000.onmicrosoft.com>
2023-12-15 10:01:24 +0100

    ex20

---
+++ stagiaires/Simona/20-exe-array-index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$monTab = ['un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf', 'dix'];
+echo $element[0] . '<hr>';
+echo $element[1] . '<hr>';
+echo $element[2] . '<hr>';
+echo $element[3] . '<hr>';
+echo $element[4] . '<hr>';
+echo $element[5] . '<hr>';
+echo $element[6] . '<hr>';
+echo $element[7] . '<hr>';
+echo $element[8] . '<hr>';
+echo $element[9] . '<hr>';
+?>
